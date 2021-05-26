78521043f888ed5d1fc0ab0d9779f06939f53a64
Michael Adenew <76660132+MichaelAdenew@users.noreply.github.com>
2021-05-26 09:37:24 -0400

    Create 1 Temp Sensor, no LCD.ino

---
+++ Temp_Sensor_ variations/1_sensor_and_LCD/1 Temp Sensor, no LCD.ino
@@ -0,0 +1,27 @@
+//Include libraries
+// #include <LiquidCrystal.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// LiquidCrystal lcd(8, 9, 4, 5, 6, 7); //Sets pins used by default
+
+OneWire oneWire2(2);                  //Sets the pin for temperature sensor at 2
+
+DallasTemperature sensors2(&oneWire2); //Sends information from OneWire to DallasTemperature
+
+void setup(){
+//   lcd.begin(16, 2);               //Start the LCD
+//   lcd.setCursor(0,0);             //Sets the cursor location 
+//   lcd.print("Temp:");//Prints temperature
+pinMode(10,OUTPUT);
+digitalWrite(10,LOW);
+//  lcd.setCursor(0,1);
+}
+ 
+void loop(){
+//   lcd.setCursor(6,0);             //Cursor is moved to second line
+  sensors2.requestTemperatures();  //Call temperature from sensor
+  Serial.print(sensors2.getTempCByIndex(0)); //Prints temperature
+  
+  delay(5000);                    //Delays next temperature update by one second
+}
