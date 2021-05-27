da00ed073e244fdcc518838d191fb9c7f31ee71f
Michael Adenew <76660132+MichaelAdenew@users.noreply.github.com>
2021-05-27 09:44:30 -0400

    LCD.begin() modification

--- Arduino Only Variations/Arduino_No_LCD.ino
+++ Arduino Only Variations/Arduino_No_LCD.ino
@@ -1,5 +1,11 @@
-	void setup() {
+//Include libraries
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7); //Sets pins used by default
+
+
+void setup() {
   // put your setup code here, to run once:
+lcd.begin(16, 2);               //Start the LCD
 
 }
 
